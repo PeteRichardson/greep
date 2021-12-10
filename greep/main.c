4b74e441201544594c0097433dcfb30332fe55ab
Pete Richardson <pete@peterichardson.com>
2021-12-10 15:32:07 -0800

    Now can read from file:   greep <string> [filename] but I broke stdin... seg fault

--- greep/main.c
+++ greep/main.c
@@ -8,29 +8,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "utils.h"
+#include <sys/syslimits.h>
+#include <fcntl.h>
+
+#define SEP_CHARS "//"
+
+typedef struct OPTIONS {
+    char *search_word;
+    FILE *stream;       // default to 0 (stdin)
+} OPTIONS;
 
 
-int main(int argc, char *argv[]) {
-    const char *search_word;
+// Returns file leaf name from abs path
+const char *leaf_name(char *path) {
+    char *token = strtok(path, SEP_CHARS);
+    char *prev_token = "greep";
+    while (token != NULL) {
+        prev_token = token;
+        token = strtok(NULL, SEP_CHARS);  // this will be null after the last '/'
+    }
+    return prev_token;
+}
+
+
+void find(char *search_word, FILE *stream, void (*found_callback)(unsigned long line, char *search_word)) {
     unsigned long search_word_maxindex;
     
-    if (argc != 2) {
-        const char *exe_name = leaf_name(argv[0]);
-        printf("usage: %s <string>\n", exe_name);
-        exit(EXIT_FAILURE);
-        
-    }
-    search_word = argv[1];
     search_word_maxindex = strlen(search_word) - 1;
-    printf("# Searching for %s\n", search_word);
-    //printf("# First char: %c\n", search_word[0]);
-    
+
     char c = '\0';
     unsigned long i = 0;
     unsigned long lineno = 1;
     while (1) {
-        c = getchar();
+        c = fgetc(stream);
         if (c == '\n') {
             lineno++;
         }
@@ -41,19 +51,57 @@ int main(int argc, char *argv[]) {
         if (i != 0 && c != search_word[i]) {
             i = 0;
             ungetc(c, stdin);
-            //printf("[nope]\n");
+//            printf("[nope]\n");
             continue;
         }
         if (c == search_word[i]) {
             if (i==search_word_maxindex ) {
-                printf("# Found it: %5lu: %s\n", lineno, search_word);
+                (*found_callback)(lineno, search_word);
                 i = 0;
                 continue;
             }
             i++;
         }
-        
-    
     }
+}
+
+void found_callback(unsigned long line_num, char *search_word) {
+    printf("%3lu: %s\n", line_num, search_word);
+}
+
+
+void parse_options(OPTIONS *options, int argc, char *argv[]) {
+    char *stream_name = "/dev/stdin";
+    options->stream = 0;
+    switch(argc) {
+        case 3 :
+            stream_name = argv[2];
+            options->stream = fopen(stream_name, "r");
+            if (options->stream == NULL) {
+                fprintf(stderr, "Unable to open file '%s' for reading.\n", argv[2]);
+                exit(EXIT_FAILURE);
+            }
+            /* intentionally falling through to get search_word */
+        case 2 :
+             options->search_word = argv[1];
+             break;
+ 
+        default :
+            printf("usage: %s <string> [file]\n", leaf_name(argv[0]));
+            exit(EXIT_FAILURE);
+    }
+    
+    printf("# Searching for '%s'\n", options->search_word);
+    printf("# Searching in '%s'\n", stream_name);
+
+}
+
+int main(int argc, char *argv[]) {
+    // usage:  greep <string_to_find> [file_to_search]
+    OPTIONS options;
+    parse_options(&options, argc, argv);
+    
+    find(options.search_word, options.stream, found_callback);
+    
     exit(EXIT_SUCCESS);
 }
