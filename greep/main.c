fdde23cfa9c02da34aed1168bcc2635ce4c3f09a
Pete Richardson <pete@peterichardson.com>
2021-12-11 21:31:52 -0800

    Fix infinite loop when first char of search_word is repeated in file being searched.
    Also moved file open to find()
    Removed unused code from options.c

--- greep/main.c
+++ greep/main.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include "options.h"
 
-const char *argp_program_version     = "greep 0.3";
+const char *argp_program_version     = "greep 0.4";
 const char *argp_program_bug_address = "<pete@peterichardson.com>";
 
 /*
@@ -20,23 +20,28 @@ const char *argp_program_bug_address = "<pete@peterichardson.com>";
  TODO: read stream a line at a time, and output the whole line
  */
 void find(char *search_word,
-          FILE *stream,
           char *filename,
           void (*found_callback)(char *, unsigned long, char *)) {
     
+    FILE *stream = fopen(filename, "r");
+    if (stream == NULL) {
+        fprintf(stderr, "# ERROR: Unable to open file '%s' for reading.\n", filename);
+        return;
+    }
+
     unsigned long search_word_maxindex = strlen(search_word) - 1;
 
     char c = '\0';
     unsigned long i = 0;
     unsigned long lineno = 1;
     while ((c = fgetc(stream)) != EOF) {
+//        printf("%c, %lu\n", c, i);
         if (c == '\n') lineno++;
         
         if (i != 0 && c != search_word[i]) {
             i = 0;
             ungetc(c, stream);
-        }
-        if (c == search_word[i]) {
+        } else if (c == search_word[i]) {
             if (i==search_word_maxindex ) {
                 i = 0;
                 (*found_callback)(filename, lineno, search_word);
@@ -44,6 +49,7 @@ void find(char *search_word,
             i++;
         }
     }
+    fclose(stream);
 }
 
 // Run this code whenever a match is found
@@ -55,7 +61,7 @@ int main(int argc, char *argv[]) {
     arguments_t args = ARGUMENT_DEFAULT_VALUES;
     int err = argp_parse (&argp, argc, argv, 0, 0, &args);
     if (err != 0) {
-        fprintf(stderr, "unabled to parse command line options: %d", err);
+        fprintf(stderr, "# ERROR: Unable to parse command line options: %d", err);
         exit(EXIT_FAILURE);
     }
     
@@ -64,18 +70,8 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "# Processing %d files: %s...%s\n", args.filecount, args.filenames[0], args.filenames[args.filecount - 1]);
     }
     
-    FILE *stream;
-    char *filename;
     for (int i = 0; i < args.filecount; i++ ) {
-        filename = args.filenames[i];
-        // TODO: Move fopen into the find() function.  Easier to move to a pthread
-        // TODO: Close the stream
-        stream = fopen(filename, "r");
-        if (stream == NULL) {
-            fprintf(stderr, "# ERROR: Unable to open file '%s' for reading.\n", filename);
-            continue;
-        }
-        find(args.search_word, stream, filename, found_callback);
+         find(args.search_word, args.filenames[i], found_callback);
     }
 
     exit(EXIT_SUCCESS);
--- greep/options.c
+++ greep/options.c
@@ -10,34 +10,16 @@
 
 #include <string.h>
 
-#define SEP_CHARS "//"
-
-// Returns file leaf name from abs path
-const char *leaf_name(char *path) {
-    char *token = strtok(path, SEP_CHARS);
-    char *prev_token = "greep";
-    while (token != NULL) {
-        prev_token = token;
-        token = strtok(NULL, SEP_CHARS);  // this will be null after the last '/'
-    }
-    return prev_token;
-}
-
 error_t parse_opt (int key, char *arg, struct argp_state *state)
 {
-    /* Get the input argument from argp_parse, which we
-     know is a pointer to our arguments structure. */
     struct arguments_t *args = state->input;
-    //fprintf(stderr, "arg: %s\n", arg);
 
     switch (key) {
         case 'v':
-            //fprintf(stderr, "\t%d. v: %s\n", state->arg_num, arg);
             args->verbose = 1;
             break;
             
         case 's':
-            //fprintf(stderr, "\t%d. s: %s\n", state->arg_num, arg);
             args->search_word = arg;
             break;
  
