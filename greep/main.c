a7052392e292bddc7d7153ae9f3eae06ad38986f
Pete Richardson <pete@peterichardson.com>
2021-12-12 15:58:36 -0800

    Added threading (using pthreads)

--- greep/main.c
+++ greep/main.c
@@ -8,10 +8,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
 #include "options.h"
 
-const char *argp_program_version     = "greep 0.4";
+const char *argp_program_version     = "greep 0.5";
 const char *argp_program_bug_address = "<pete@peterichardson.com>";
+
+typedef struct threadargs_t {
+    const char *search_word;
+    const char *filename;
+    void (*found_callback)(const char *, unsigned long, const char *);
+} threadargs_t;
 
 /*
  Find a word in a stream of chars.
@@ -19,9 +28,9 @@ const char *argp_program_bug_address = "<pete@peterichardson.com>";
  TODO: memory map the file.  Maybe faster?
  TODO: read stream a line at a time, and output the whole line
  */
-void find(char *search_word,
-          char *filename,
-          void (*found_callback)(char *, unsigned long, char *)) {
+void find(const char *search_word,
+          const char *filename,
+          void (*found_callback)(const char *, unsigned long, const char *)) {
     
     FILE *stream = fopen(filename, "r");
     if (stream == NULL) {
@@ -35,7 +44,7 @@ void find(char *search_word,
     unsigned long i = 0;
     unsigned long lineno = 1;
     while ((c = fgetc(stream)) != EOF) {
-//        printf("%c, %lu\n", c, i);
+        //  printf("%s: %c, %lu\n", filename, c, i);
         if (c == '\n') lineno++;
         
         if (i != 0 && c != search_word[i]) {
@@ -44,7 +53,7 @@ void find(char *search_word,
         } else if (c == search_word[i]) {
             if (i==search_word_maxindex ) {
                 i = 0;
-                (*found_callback)(filename, lineno, search_word);
+                (*found_callback)(filename, lineno, search_word);  // for now, log word. Switch to whole line later.
             }
             i++;
         }
@@ -52,9 +61,16 @@ void find(char *search_word,
     fclose(stream);
 }
 
+void *threaded_find(void *arg) {
+    threadargs_t thread_args = *(threadargs_t *) arg;
+    find(thread_args.search_word, thread_args.filename, thread_args.found_callback);
+    return 0;
+}
+
+
 // Run this code whenever a match is found
-void found_callback(char *filename, unsigned long line_num, char *line) {
-    printf("%s:%lu %s\n", filename, line_num, line);
+void found_callback(const char *filename, unsigned long line_num, const char *line) {
+    fprintf(stderr, "%s:%lu %s\n", filename, line_num, line);
 }
 
 int main(int argc, char *argv[]) {
@@ -67,12 +83,34 @@ int main(int argc, char *argv[]) {
     
     if (args.verbose) {
         fprintf(stderr, "# Searching for '%s'\n", args.search_word);
-        fprintf(stderr, "# Processing %d files: %s...%s\n", args.filecount, args.filenames[0], args.filenames[args.filecount - 1]);
     }
     
+    pthread_t threads[args.filecount+5];
+    threadargs_t payloads[args.filecount];
     for (int i = 0; i < args.filecount; i++ ) {
-         find(args.search_word, args.filenames[i], found_callback);
+        threadargs_t thread_args = {
+            .search_word = args.search_word,
+            .filename = args.filenames[i],
+            .found_callback = found_callback
+        };
+        payloads[i] = thread_args;
+    }
+        
+    err = 0;
+    for (int i = 0; i < args.filecount; i++ ) {
+        if (args.verbose) {
+            fprintf(stderr, "# Processing file %d: %s\n", i, args.filenames[i]);
+        }
+        err = pthread_create(&threads[i], NULL, &threaded_find, &payloads[i]);
+        if (err) {
+            printf("%s\n", strerror(err), stderr);
+            exit(EXIT_FAILURE);
+        }
     }
 
+    for (int i=0; i<args.filecount; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    
     exit(EXIT_SUCCESS);
 }
