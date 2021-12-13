c95c5b7bdc23b988d2bcd9448838edf9615dfb48
Pete Richardson <pete@peterichardson.com>
2021-12-13 14:12:20 -0800

    Setup scaffolding for memory mapped files and different search algorithms

--- greep/main.c
+++ greep/main.c
@@ -13,26 +13,36 @@
 
 #include "options.h"
 
-const char *argp_program_version     = "greep 0.5";
+const char *argp_program_version     = "greep 0.6";
 const char *argp_program_bug_address = "<pete@peterichardson.com>";
+
+
+typedef void (*callback_t) (const char *, unsigned long, const char *);
+
+typedef void (*search_alg_t) (const char *,         // search_word
+                              const char *,         // filename
+                              const char *,         // start
+                              const unsigned long,  // length
+                              callback_t *);        // callback
+
 
 typedef struct threadargs_t {
     const char *search_word;
     const char *filename;
-    void (*found_callback)(const char *, unsigned long, const char *);
+    search_alg_t search_alg;
+    callback_t found_callback;
 } threadargs_t;
 
 /*
- Find a word in a stream of chars.
- 
- TODO: memory map the file.  Maybe faster?
- TODO: use better string search algorithm.
-        see http://www-igm.univ-mlv.fr/~lecroq/string/index.html
- */
-void find(const char *search_word,
-          const char *filename,
-          void (*found_callback)(const char *, unsigned long, const char *)) {
-    
+ Find using brute force algorithm
+    assume file is in memory
+*/
+void find_bf(const char *search_word,
+             const char *filename,
+             const char *start,
+             const unsigned long length,
+             callback_t *found_callback) {
+    // mmap file
     FILE *stream = fopen(filename, "r");
     if (stream == NULL) {
         fprintf(stderr, "# ERROR: Unable to open file '%s' for reading.\n", filename);
@@ -60,12 +70,18 @@ void find(const char *search_word,
             i++;
         }
     }
+    
     fclose(stream);
+
 }
 
 void *threaded_find(void *arg) {
     threadargs_t thread_args = *(threadargs_t *) arg;
-    find(thread_args.search_word, thread_args.filename, thread_args.found_callback);
+    
+    // mmap file
+    (thread_args.search_alg)(thread_args.search_word, thread_args.filename, 0, 0, &thread_args.found_callback);
+    // munmap file
+
     return 0;
 }
 
@@ -93,6 +109,7 @@ int main(int argc, char *argv[]) {
         threadargs_t thread_args = {
             .search_word = args.search_word,
             .filename = args.filenames[i],
+            .search_alg = find_bf,
             .found_callback = found_callback
         };
         payloads[i] = thread_args;
